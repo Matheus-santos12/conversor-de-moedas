168d7c4d8edc20b0017bdf9d2e3e45791be33a98
Matheus Silva <rematheussilva@gmail.com>
2026-01-13 17:12:14 -0500

    Colocando os valores das moedas para realizar a conversão

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+// cotação de moedas.
+const USD = 5.37;
+const EUR = 6.26;
+const GBP = 7.22;
+const PEN = 1.6;
+const KWD = 17.45;
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
@@ -13,5 +20,26 @@ amount.addEventListener("input", () => {
 form.onsubmit = () => {
   event.preventDefault();
 
-  console.log(currency.value);
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+    case "PEN":
+      convertCurrency(amount.value, PEN, "S/");
+      break;
+    case "KWD":
+      convertCurrency(amount.value, KWD, "KD");
+      break;
+  }
 };
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol);
+}
